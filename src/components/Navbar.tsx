7abc0892f3708c9df3500d994c6bdbab5b5c46c1
Cosmin <vaschicosmin@gmail.com>
2026-06-10 15:24:09 +0300

    Increase logo to 120px, navbar to h-32

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,12 +11,12 @@ export default function Navbar() {
   return (
     <nav className="w-full bg-white border-b border-gray-100 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-24">
+        <div className="flex items-center justify-between h-32">
           <Link href="/" className="flex items-center">
             <img
               src="/kummo_assets/logo/kummo-logo.svg"
               alt="KUMMO"
-              style={{ height: '90px', width: 'auto' }}
+              style={{ height: '120px', width: 'auto' }}
             />
           </Link>
 
